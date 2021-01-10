0b1ce1209ac4fb5d2106af92f0f0bcc4cee6bef5
Paulo Coutinho <paulo@prsolucoes.com>
2021-01-10 02:08:09 -0300

    project update to new architecture

--- files/targets/android_aar/conan/recipe/conanfile.py
+++ files/targets/android_aar/conan/recipe/conanfile.py
@@ -21,7 +21,6 @@ class TargetConan(ConanFile):
         "boost:without_fiber": True,
         "boost:without_nowide": True,
         "boost:without_json": True,
-        "boost:without_stacktrace_addr2line": True,
     }
     exports_sources = "*"
     generators = "cmake"
