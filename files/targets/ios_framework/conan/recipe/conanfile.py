c9311b464eb14addd319ee28d39952d520c35771
Paulo Coutinho <paulo@prsolucoes.com>
2021-01-10 00:24:32 -0300

    project update to new architecture

--- files/targets/ios_framework/conan/recipe/conanfile.py
+++ files/targets/ios_framework/conan/recipe/conanfile.py
@@ -23,7 +23,7 @@ class TargetConan(ConanFile):
         "boost:without_fiber": True,
         "boost:without_nowide": True,
         "boost:without_json": True,
-        "boost:without_boost_locale": True,
+        "boost:without_locale": True,
         "boost:without_stacktrace_addr2line": True,
     }
     exports_sources = "*"
